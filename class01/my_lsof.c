6a2f540902cd3f37b55b2afa4ea06c626a0e08f8
ArtKomarov <Komarov.A@phystech.edu>
2021-10-05 12:44:30 +0300

    lsof: readlinks from fd, map_files | add perror

--- class01/my_lsof.c
+++ class01/my_lsof.c
@@ -6,6 +6,8 @@
 #include <unistd.h>
 #include <string.h>
 
+void readlinks(char* pid, char* dir_name);
+
 int main() {
     printf("pid \t inode \t\t\t pathname\n");
     regex_t regex;
@@ -19,46 +21,56 @@ int main() {
     struct dirent *dir;
     proc_dir = opendir("/proc");
 
-    if (proc_dir) {
-        while ((dir = readdir(proc_dir)) != NULL) {
-            if (regexec(&regex, dir->d_name, 0, NULL, 0) == 0) {
-                 // Open and read from /proc/[PID]/maps
-                 char mem_path[256+11];
-                 sprintf(mem_path, "/proc/%s/maps", dir->d_name);
-                 FILE* fd = fopen(mem_path, "r");
-
-                 if (fd == NULL) {
-                     continue;
-                 }
-
-                 char mem_line[1024];
-
-                 while (fgets(mem_line, 2047, fd) != NULL) {
-                     char* space = mem_line;
-
-                     int i;
-                     // Skip to inode
-                     for (i = 0; i < 4; ++i)
-                         space = strchr(space, ' ') + 1;
-
-                     char* next_space = strchr(space, ' ');
-                     *next_space = '\0';
-
-                     if (strcmp(space, "0") == 0)
-                         continue;
-
-                     // Write info on console
-                     printf("%s ", dir->d_name);
-                     printf("%s ", space);
-                     printf("%s", next_space + 1);
-                 }
-
-                 if (fclose(fd) != 0)
-                     fputs("Error in closing file!", stderr);
-            }
-        }
-        closedir(proc_dir);
+    if (proc_dir == NULL) {
+        perror("opendir(/proc)");
+        return -1;
     }
+
+    while ((dir = readdir(proc_dir)) != NULL) {
+        if (regexec(&regex, dir->d_name, 0, NULL, 0) != 0)
+            continue;
+
+        readlinks(dir->d_name, "fd");
+        readlinks(dir->d_name, "map_files");
+    }
+
+    if (closedir(proc_dir) != 0)
+        fputs("Error in closing proc directory!", stderr);
+
 
     return 0;
 }
+
+void readlinks(char* pid, char* dir_name) {
+    // Open and read links from /proc/[PID]/dir_name
+    const int files_path_len = 256 + 64;
+    char files_path[files_path_len];
+    const int dir_path_len = snprintf(files_path, files_path_len - 1, "/proc/%s/%s/", pid, dir_name);
+
+    DIR* files_dir;
+    struct dirent *file;
+    files_dir = opendir(files_path);
+
+    if (files_dir == NULL)
+        return;
+
+    while ((file = readdir(files_dir)) != NULL) {
+        files_path[dir_path_len] = '\0';
+        char link_name[files_path_len];
+
+        ssize_t read_bytes = readlink(strcat(files_path, file->d_name), link_name, files_path_len - 1);
+        if (read_bytes <= 0)
+            continue;
+
+        link_name[read_bytes] = '\0';
+
+        // Write info on console
+        printf("%s ", pid);
+        printf("%s\n", link_name);
+    }
+
+    if (closedir(files_dir) != 0)
+        fprintf(stderr, "Error in closing %s directory!", dir_name);
+}
+
+
--- class01/my_ps.c
+++ class01/my_ps.c
@@ -19,71 +19,69 @@ int main() {
     struct dirent *dir;
     proc_dir = opendir("/proc");
 
-    if (proc_dir) {
-        puts("PID CMD");
+    if (proc_dir == NULL) {
+        perror("opendir(/proc)");
+        return -1;
+    }
+    puts("PID CMD");
 
-        while ((dir = readdir(proc_dir)) != NULL) {
-            if (regexec(&regex, dir->d_name, 0, NULL, 0) == 0) {
-                 printf("%s ", dir->d_name);
+    while ((dir = readdir(proc_dir)) != NULL) {
+        if (regexec(&regex, dir->d_name, 0, NULL, 0) != 0)
+            continue;
+        printf("%s ", dir->d_name);
 
-                 // Open and read from /proc/[PID]/cmdline
-                 const int comm_len = 256 + 11;
-                 char comm_path[comm_len];
-                 sprintf(comm_path, "/proc/%s/cmdline", dir->d_name);
-                 int fd = open(comm_path, O_RDONLY);
+        // Open and read from /proc/[PID]/cmdline
+        const int comm_len = 256 + 11;
+        char comm_path[comm_len];
+        sprintf(comm_path, "/proc/%s/cmdline", dir->d_name);
+        int fd = open(comm_path, O_RDONLY);
 
-                 if (fd < 0) {
-                     printf(" \n");
-                     continue;
-                 }
-
-                 int bytes_read = read(fd, comm_path, comm_len - 1);
-
-                 if (bytes_read > 0) {
-                     comm_path[bytes_read] = '\0';
-
-                     char* space = strchr(comm_path, ' ');
-                     if (space != NULL)
-                         *space = '\0';
-
-                     printf("%s\n", comm_path);
-                 } else if (bytes_read == 0) {
-                     if (close(fd) != 0)
-                         fputs("Error in closing file!", stderr);
-
-                     // Open and read from /proc/[PID]/comm
-                     sprintf(comm_path, "/proc/%s/comm", dir->d_name);
-                     fd = open(comm_path, O_RDONLY);
-
-                     if (fd < 0) {
-                         printf(" \n");
-                         continue;
-                     }
-
-                     bytes_read = read(fd, comm_path, comm_len - 1);
-
-                     if (bytes_read > 0) {
-                         comm_path[bytes_read] = '\0';
-                         printf("%s", comm_path);
-                     } else {
-                         printf(" \n");
-                     }
-                 } else {
-                     printf(" \n");
-                 }
-
-                 if (close(fd) != 0)
-                     fputs("Error in closing file!", stderr);
-            }
+        if (fd < 0) {
+            printf(" \n");
+            continue;
         }
 
-        closedir(proc_dir);
+        int bytes_read = read(fd, comm_path, comm_len - 1);
+
+        if (bytes_read > 0) {
+            comm_path[bytes_read] = '\0';
+
+            char* space = strchr(comm_path, ' ');
+            if (space != NULL)
+                *space = '\0';
+
+            printf("%s\n", comm_path);
+        } else if (bytes_read == 0) {
+            if (close(fd) != 0)
+                fputs("Error in closing file!", stderr);
+
+            // Open and read from /proc/[PID]/comm
+            sprintf(comm_path, "/proc/%s/comm", dir->d_name);
+            fd = open(comm_path, O_RDONLY);
+
+            if (fd < 0) {
+                printf(" \n");
+                continue;
+            }
+
+            bytes_read = read(fd, comm_path, comm_len - 1);
+
+            if (bytes_read > 0) {
+                comm_path[bytes_read] = '\0';
+                printf("%s", comm_path);
+            } else {
+                printf(" \n");
+            }
+        } else {
+            printf(" \n");
+        }
+
+        if (close(fd) != 0)
+            fputs("Error in closing file!", stderr);
+
     }
 
-    /* if (chdir("/proc") == -1) {
-        fputs("Can not change directory to /proc!\n", stderr);
-        return -1;
-    } */
+    closedir(proc_dir);
 
     return 0;
 }
